f957cd4e593da76c18555bcf025d9f8f2937ab6f
Roeland Jago Douma <unix@rullzer.com>
2011-03-12 08:56:36 +0100

    Make notes readonly untill we can do editing

--- resource/facebookresource.cpp
+++ resource/facebookresource.cpp
@@ -543,8 +543,7 @@ void FacebookResource::retrieveCollections()
   notes.setName( i18n( "Notes" ) );
   notes.setParentCollection( Akonadi::Collection::root() );
   notes.setContentMimeTypes( QStringList() << "text/x-vnd.akonadi.note"  );
-  notes.setRights( Collection::ReadOnly      | Collection::CanChangeItem | 
-                   Collection::CanDeleteItem | Collection::CanCreateItem );
+  notes.setRights( Collection::ReadOnly);
   EntityDisplayAttribute * const notesDisplayAttribute = new EntityDisplayAttribute();
   notesDisplayAttribute->setIconName( "facebookresource" );
   notes.addAttribute( notesDisplayAttribute );
