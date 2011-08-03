fc7648424b058947f62e8e1b06f37c809b3c67af
Alexander Potashev <aspotashev@gmail.com>
2011-08-03 19:49:51 +0400

    Add CreateAlbumJob

---
+++ libkvkontakte/createalbumjob.cpp
@@ -0,0 +1,45 @@
+/* Copyright 2011 Alexander Potashev <aspotashev@gmail.com>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+#include "createalbumjob.h"
+#include <qjson/qobjecthelper.h>
+
+CreateAlbumJob::CreateAlbumJob(const QString &accessToken,
+                               const QString &title, const QString &description,
+                               int privacy, int comment_privacy)
+    : VkontakteJob("photos.createAlbum", accessToken)
+{
+    addQueryItem("title", title);
+    if (!description.isEmpty())
+        addQueryItem("description", description);
+    if (privacy != AlbumInfo::PRIVACY_UNKNOWN)
+        addQueryItem("privacy", QString::number(privacy));
+    if (comment_privacy != AlbumInfo::PRIVACY_UNKNOWN)
+        addQueryItem("comment_privacy", QString::number(comment_privacy));
+}
+
+void CreateAlbumJob::handleData(const QVariant &data)
+{
+    m_album = AlbumInfoPtr(new AlbumInfo());
+    QJson::QObjectHelper::qvariant2qobject(data.toMap(), m_album.data());
+}
+
+AlbumInfoPtr CreateAlbumJob::album() const
+{
+    return m_album;
+}
---
+++ libkvkontakte/createalbumjob.h
@@ -0,0 +1,42 @@
+/* Copyright 2011 Alexander Potashev <aspotashev@gmail.com>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+#ifndef CREATEALBUMJOB_H
+#define CREATEALBUMJOB_H
+
+#include "vkontaktejobs.h"
+#include "albuminfo.h"
+
+class LIBKVKONTAKTE_EXPORT CreateAlbumJob : public VkontakteJob
+{
+public:
+    CreateAlbumJob(const QString &accessToken,
+                   const QString &title, const QString &description = QString(),
+                   int privacy = AlbumInfo::PRIVACY_UNKNOWN,
+                   int comment_privacy = AlbumInfo::PRIVACY_UNKNOWN);
+
+    AlbumInfoPtr album() const;
+
+protected:
+    virtual void handleData(const QVariant &data);
+
+private:
+    AlbumInfoPtr m_album;
+};
+
+#endif // CREATEALBUMJOB_H
