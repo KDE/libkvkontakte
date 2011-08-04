607703bff238a13cfdae0ac21cfb5350e6f3f626
Alexander Potashev <aspotashev@gmail.com>
2011-08-04 23:17:59 +0400

    Add PhotoListJob

---
+++ libkvkontakte/photolistjob.cpp
@@ -0,0 +1,52 @@
+/* Copyright 2011 Alexander Potashev <aspotashev@gmail.com>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+#include "photolistjob.h"
+#include <qjson/qobjecthelper.h>
+
+// http://vkontakte.ru/developers.php?o=-1&p=photos.get
+//
+// The API also allows to set "limit" and "offset", but that
+// does not seem to be useful.
+PhotoListJob::PhotoListJob(const QString &accessToken,
+                           int uid, int aid, const QIntList &pids)
+    : VkontakteJob("photos.get", accessToken)
+{
+    addQueryItem("uid", QString::number(uid));
+    addQueryItem("aid", QString::number(aid));
+    if (!pids.empty())
+        addQueryItem("pids", pids.join());
+}
+
+void PhotoListJob::handleItem(const QVariant &data)
+{
+    PhotoInfoPtr item(new PhotoInfo());
+    QJson::QObjectHelper::qvariant2qobject(data.toMap(), item.data());
+    m_list.append(item);
+}
+
+void PhotoListJob::handleData(const QVariant &data)
+{
+    foreach (const QVariant &item, data.toList())
+        handleItem(item);
+}
+
+QList<PhotoInfoPtr> PhotoListJob::list() const
+{
+    return m_list;
+}
---
+++ libkvkontakte/photolistjob.h
@@ -0,0 +1,48 @@
+/* Copyright 2011 Alexander Potashev <aspotashev@gmail.com>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+#ifndef PHOTOLISTJOB_H
+#define PHOTOLISTJOB_H
+
+#include "vkontaktejobs.h"
+#include "photoinfo.h"
+#include "qintlist.h"
+
+class LIBKVKONTAKTE_EXPORT PhotoListJob : public VkontakteJob
+{
+    Q_OBJECT
+public:
+    PhotoListJob(const QString &accessToken,
+                 int uid, int aid, const QIntList &pids = QIntList());
+
+    QList<PhotoInfoPtr> list() const;
+
+protected:
+    /**
+    * @brief Handles the data returned by the VkontakteGetJob
+    * @param data A JSON string containing the data.
+    */
+    virtual void handleData(const QVariant &data);
+
+    void handleItem(const QVariant &data);
+
+private:
+    QList<PhotoInfoPtr> m_list;
+};
+
+#endif // PHOTOLISTJOB_H
